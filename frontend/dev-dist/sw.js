7f5975dd66e985ba2efd025d04d856f1a992fcb5
Aks-20 <akshatgupta0043@gmail.com>
2026-02-09 23:04:00 +0530

    chore: update express to version 4.22.1 and revise service worker cache revision

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.st353cevtno"
+    "revision": "0.rouh2k5t4mo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
