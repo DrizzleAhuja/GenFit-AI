0e3d919930745586a4eb98bd83f0f8957bf19d6c
Aks-20 <akshatgupta0043@gmail.com>
2026-02-09 22:54:17 +0530

    Refactor code structure for improved readability and maintainability

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -67,7 +67,7 @@ if (!self.define) {
     });
   };
 }
-define(['./workbox-af03485a'], (function (workbox) { 'use strict';
+define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
 
   self.skipWaiting();
   workbox.clientsClaim();
@@ -82,7 +82,7 @@ define(['./workbox-af03485a'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.cbsrjb3ca7"
+    "revision": "0.st353cevtno"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
