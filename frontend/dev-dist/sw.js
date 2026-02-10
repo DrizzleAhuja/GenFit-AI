edc8b121bf5261ec36d233bab46466cf83c610fd
Aman kumar prasad <kumarprasadaman1234@gmail.com>
2026-02-10 21:25:28 +0530

    Update Gemini API URL in config and revise service worker revision for cache management

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -67,7 +67,7 @@ if (!self.define) {
     });
   };
 }
-define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
+define(['./workbox-af03485a'], (function (workbox) { 'use strict';
 
   self.skipWaiting();
   workbox.clientsClaim();
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.c304nmsv39g"
+    "revision": "0.0qagek3774"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
